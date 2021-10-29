67bacb3d060196dc0097532f40ad8eae8887b9ee
ZemyCode <69816894+ZemyCode@users.noreply.github.com>
2021-10-29 13:30:21 -0400

    Setup the Feed page, and a global background color

--- frontend/senior-project/src/pages/Feed.js
+++ frontend/senior-project/src/pages/Feed.js
@@ -1,9 +1,34 @@
 import React from "react";
+import "../styles/Feed.css";
+import { Card } from "react-bootstrap";
 
 export default function Feed() {
-    return (     
-        <div>
-            <p>This is the feed page</p>
-        </div> 
-    );
+  return (
+    <div className="feed-body text-center col-md-12">
+      <div className="Feed-Area">
+        <h1>News Feed</h1>
+        <Card className="Feed-Box mt-3 mx-auto w-50">
+          <Card.Header className="d-flex">
+            <p className="me-auto">Stock Symbol [Date] shared by [Username]</p>
+            <button className="share-button">Share</button>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <p>
+                Preview of article Preview of article Preview of article Preview of article Preview of Preview of article Preview
+                of article Preview of article Preview of article Preview of Preview of article Preview of article Preview of
+                article Preview of article Preview of Preview of article Preview of article Preview of article Preview of article
+                Preview of Preview of article Preview of article Preview of article Preview of article Preview of Preview of
+                article Preview of article Preview of article Preview of article Preview of Preview of article Preview of article
+                Preview of article Preview of article Preview of Preview of article Preview of article Preview of article Preview
+                of article Preview of Preview of article Preview of article Preview of article Preview of article Preview of
+                Preview of article Preview of article Preview of article Preview of article Preview of{" "}
+              </p>
+            </Card.Text>
+            <button className="continue-reading-button">Continue Reading</button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 }
