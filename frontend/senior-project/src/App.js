1e3226a5ed6b05af10484a1329494b226da367cd
Bradley Wu <bradleydwu@gmail.com>
2021-11-01 20:04:39 -0400

    styled edit form
    fixed css stuff, added pfp, somewhat broken private profile slider

--- frontend/senior-project/src/App.js
+++ frontend/senior-project/src/App.js
@@ -23,8 +23,9 @@ export default function App() {
           <Container>
             <Switch>
               <Route exact path="/">
-                <Signup />
-                <Home />
+                <EditProfile />
+                {/* <Signup />
+                <Home /> */}
               </Route>
 
               <Route path="/feed">
--- frontend/senior-project/src/pages/EditProfile.js
+++ frontend/senior-project/src/pages/EditProfile.js
@@ -4,16 +4,34 @@ import '../styles/EditProfile.css'
 export default function EditProfile() {
     return (     
         <div className = 'body'>
-            <h1>Edit Profile Page</h1>
             <div className = 'EditProfile-body'>
                 <div className = 'EditProfile-box'>
+                    <h1>Edit Profile Page</h1>
                     <div className = 'EditProfile-input'>
-                        <input type = 'text' class = 'form-control' id = 'username' placeholder = 'Username'></input>
-                        <input type = 'text' class = 'form-control' id = 'firstname' placeholder = 'First Name'></input>
-                        <input type = 'text' class = 'form-control' id = 'lastname' placeholder = 'Last Name'></input>
+
+                        <div className = 'pfp'>
+                            <img src = "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" id = 'pfp-image' alt="default pfp"></img>
+                            <button className = 'button' id = 'pfp-button'>Choose Picture</button>
+                        </div>
+
+                        <input type = 'text' className = 'form-control' id = 'username' placeholder = 'Username'></input>
+                        <textarea className = 'form-control' id = 'bio-textbox' placeholder = 'User Bio' rows = '3'></textarea>
+
+                        <input type = 'text' className = 'form-control' id = 'firstname' placeholder = 'First Name'></input>
+                        <input type = 'text' className = 'form-control' id = 'lastname' placeholder = 'Last Name'></input>
+
+                        <div id = 'private-profile'>
+                            <span>Private Profile</span>
+                            <label className = "switch">
+                                <input type = "checkbox"></input>
+                                <span className = "slider round"></span>
+                            </label>
+                        </div>
+
+                        <button className = 'button' id = 'savechanges'>Save Changes</button>
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
     );
 }
