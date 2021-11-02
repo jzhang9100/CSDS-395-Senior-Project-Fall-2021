4635b4fb19e99a434e1f29548d9260022f8b9573
Aman <abhriguvansh99@gmail.com>
2021-11-01 19:50:10 -0700

    user profile page

--- frontend/senior-project/src/App.js
+++ frontend/senior-project/src/App.js
@@ -1,52 +1,51 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Feed from "./pages/Feed";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import Search from "./pages/Search";
-import Signup from "./pages/Signup";
-import Stock from "./pages/Stock";
-import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Feed from './pages/Feed';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+import Signup from './pages/Signup';
+import Stock from './pages/Stock';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 export default function App() {
   return (
     <>
-      <div className="App">
+      <div className='App'>
         <Router>
           <NavBar />
-          {/*<Login />*/}
           <Container>
             <Switch>
-              <Route exact path="/">
+              <Route exact path='/'>
                 <Signup />
                 <Home />
               </Route>
 
-              <Route path="/feed">
+              <Route path='/feed'>
                 <Feed />
               </Route>
 
-              <Route path="/login">
+              <Route path='/login'>
                 <Login />
               </Route>
 
-              <Route path="/profile">
+              <Route path='/profile'>
                 <Profile />
               </Route>
 
-              <Route path="/search">
+              <Route path='/search'>
                 <Search />
               </Route>
 
-              <Route path="/signup">
+              <Route path='/signup'>
                 <Signup />
               </Route>
 
-              <Route path="/stock">
+              <Route path='/stock'>
                 <Stock />
               </Route>
             </Switch>
