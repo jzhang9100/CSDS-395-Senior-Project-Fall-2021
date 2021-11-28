115ca1b739c9b18f2b39be6e5aef3786a86d9c44
stackr10 <ryanstack@outlook.com>
2021-11-28 16:35:22 -0500

    fixed app.js

--- frontend/senior-project/src/App.js
+++ frontend/senior-project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Feed from './pages/Feed';
 import Home from './pages/Home';
