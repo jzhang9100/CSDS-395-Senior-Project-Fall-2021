0ffb27071bb36ae65be6aacbaa9468c002940e55
Bradley Wu <bradleydwu@gmail.com>
2021-12-07 18:57:41 -0500

    login page

--- frontend/senior-project/src/pages/Login.js
+++ frontend/senior-project/src/pages/Login.js
@@ -2,35 +2,78 @@ import React from 'react';
 import { Card, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import '../styles/Login.css';
-export default function Login() {
-  return (
-    <div className='login-body'>
-      <div className='login-box'>
-        <h1>Log In</h1>
-        <div className='input-box'>
-          <input
-            type='text'
-            className='login-button'
-            id='username'
-            placeholder='Username'
-          />
-          <input
-            type='text'
-            className='login-button'
-            id='password'
-            placeholder='Password'
-          />
-          <button id='submit-button'>Log In</button>
-        </div>
-        <div className = "register-link">
-          <p>New to Streads?</p>
-          <LinkContainer to="/signup" className="my-2 ms-4 shadow">
-            <Nav.Link className="register-button">
-              <p className="register-text">Register Here!</p>
-            </Nav.Link>
-          </LinkContainer>
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+  onUsernameChange(event) {
+    this.setState({username: event.target.value});
+      console.log(event.target.value);
+  }
+
+  onPasswordChange(event) {
+    this.setState({password: event.target.value});
+      console.log.log(event.target.value);
+  }
+
+  handleSubmit(event) {
+    fetch('http://localhost:3001/api/response', {
+      method: 'POST',
+      headers: {
+        'Accept': 'applicatoin/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    }).this((response) => {console.log(response)});
+  }
+
+  render() {
+
+    return (
+      <div className='login-body'>
+        <div className='login-box'>
+          <h1>Log In</h1>
+          <div className='input-box'>
+            <input
+              type='text'
+              className='login-button'
+              id='username'
+              placeholder='Username'
+              onChange = {this.onUsernameChange}
+            />
+            <input
+              type='text'
+              className='login-button'
+              id='password'
+              placeholder='Password'
+              onChange = {this.onPasswordChange}
+            />
+            <button id='submit-button' onClick = {this.handleSubmit}>Log In</button>
+          </div>
+          <div className = "register-link">
+            <p className = "register-text">New to Streads?</p>
+            <LinkContainer to="/signup" className="">
+              <Nav.Link className="register-button">
+                <p className="register-button-text">Register Here!</p>
+              </Nav.Link>
+            </LinkContainer>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Login;
