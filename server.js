c956a7b439e5b3ccbefc821b18a7df67264cd740
stackr10 <ryanstack@outlook.com>
2021-12-10 15:11:23 -0500

    posts endpoint

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var register = require('./endpoints/register')
 var login = require('./endpoints/login')
 var logout = require('./endpoints/logout')
 var loginToken = require('./endpoints/loginToken')
+var posts = require('./endpoints/post')
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -24,6 +25,7 @@ app.use('/login', login)
 app.use('/logout', logout)
 app.use('/loginToken', loginToken)
 app.use('/profiles', profiles)
+app.use('/posts', posts)
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
