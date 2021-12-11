30b0b8b0f7217e10f28510f930a01b938259fb59
Bradley Wu <bradleydwu@gmail.com>
2021-12-10 19:29:44 -0500

    fetch and put set up
    not tested yet

--- frontend/senior-project/src/pages/EditProfile.js
+++ frontend/senior-project/src/pages/EditProfile.js
@@ -1,6 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/EditProfile.css'
 
+async function editProfile(editProfileJson) {
+    return fetch("http://localhost:3001/login", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editProfileJson),
+    })
+        .then((response) => response.json())
+        .then((response) => response.token);
+}
+
+export default function EditProfile({ token }) {
+
+    const[profilepic, setProfilePic] = useState();
+    const[username, setUsername] = useState();
+    const[password, setPassword] = useState();
+    const[bio, setBio] = useState();
+    const[fname, setFName] = useState();
+    const[lname, setLName] = useState();
+
+    function updateProfile() {
+        fetch(`http://localhost:3001/profiles?token=${token}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response["user_data"][0]);
+      });
+    }
+
+    useEffect(() => {
+        updateProfile();
+    }, []);
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const newToken = await editProfile( {
+            profilepic,
+            username,
+            password,
+            bio,
+            fname,
+            lname,
+        });
+        token(newToken);
+        window.location = "/"
+    }
+
+    return (     
+        <div className = 'body'>
+            <div className = 'EditProfile-body'>
+                <div className = 'EditProfile-box'>
+                    <h1>Edit Profile Page</h1>
+                    <div className = 'EditProfile-input'>
+
+                        <div className = 'pfp'>
+                            <img src = "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" id = 'pfp-image' alt="default pfp"></img>
+                            <input type = 'text' className = 'form-control' id = 'profilepic' placeholder = 'Profile Picture Link' onChange = {(e) => setProfilePic(e.target.value)}></input>
+                        </div>
+
+                        <input type = 'text' className = 'form-control' id = 'username' placeholder = 'Username' onChange = {(e) => setUsername(e.target.value)}></input>
+                        <input type = 'password' className = 'form-control' id = 'password' placeholder = 'Password' onChange = {(e) => setPassword(e.target.value)}></input>
+                        <textarea className = 'form-control' id = 'bio-textbox' placeholder = 'User Bio' rows = '3' onChange = {(e) => setBio(e.target.value)}></textarea>
+
+                        <input type = 'text' className = 'form-control' id = 'firstname' placeholder = 'First Name' onchange = {(e) => setFName(e.target.value)}></input>
+                        <input type = 'text' className = 'form-control' id = 'lastname' placeholder = 'Last Name' onChange = {(e) => setLName(e.target.value)}></input>
+                        
+                        <button className = 'button' id = 'savechanges' onClick = {handleSubmit}>Save Changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+/*
 class EditProfile extends React.Component {
     constructor(props) {
         super(props);
@@ -83,7 +158,6 @@ class EditProfile extends React.Component {
 
                             <div className = 'pfp'>
                                 <img src = "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" id = 'pfp-image' alt="default pfp"></img>
-                                {/* <button className = 'button' id = 'pfp-button'>Choose Picture</button> */}
                                 <input type = 'text' className = 'form-control' id = 'profilepic' placeholder = 'Profile Picture Link' onChange = {this.onProfilePicChange}></input>
                             </div>
 
@@ -104,3 +178,4 @@ class EditProfile extends React.Component {
 }
 
 export default EditProfile;
+*/
